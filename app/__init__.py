1f85fa278476c77cac26fd181fe66d83490a3391
CyberPanther232 <kinneyh27@gmail.com>
2025-07-19 23:09:07 -0400

    Password generation frontend and backend created successfully

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,4 @@ from flask import Flask
 import os
 
 application = Flask(__name__)
+application.secret_key = "sn3&ky_sN@k3!"
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,25 @@
 from app import application
-from flask import render_template, url_for, request
+from flask import render_template, request, flash
+import secrets
+import string
+
+def generate_random_password(length, upper, lower, numbers, symbols):
+    valid_characters = ""
+        
+    if upper:
+        valid_characters += string.ascii_uppercase
+    
+    if lower:
+        valid_characters += string.ascii_lowercase
+        
+    if numbers:
+        valid_characters += string.digits
+    
+    if symbols:
+        valid_characters += string.punctuation
+    
+    return "".join(secrets.choice(valid_characters) for i in range(0, length + 1))
+
 
 @application.route("/", methods=["GET"])
 def index():
@@ -8,4 +28,20 @@ def index():
 @application.route("/generate-password", methods=["POST"])
 def generate_password():
     
+    length = int(request.form.get("length", 16)) # 16 is default if no value is provided
+    upper = "uppercase" in request.form
+    lower = "lowercase" in request.form
+    numbers = "numbers" in request.form
+    symbols = "symbols" in request.form
+    
+    try:
+        password = generate_random_password(length, upper, lower, numbers, symbols)
+        return render_template("index.html", generated_password=password)
+
+    except IndexError:
+        flash("Please click a checkbox below to add options for password generation!")
+        return render_template("index.html")
+
+@application.route("/check-strength", methods=["POST"])
+def check_strength():
     return render_template("index.html")
