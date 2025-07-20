6dc0c527cc4a601dbdcd4c072f0c9854bc6a12c2
CyberPanther232 <kinneyh27@gmail.com>
2025-07-20 12:33:47 -0400

    Finished adding Password Checker functionality

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import application
-from flask import render_template, request, flash
+from flask import render_template, request, flash, redirect
 import secrets
 import string
 
@@ -25,23 +25,81 @@ def generate_random_password(length, upper, lower, numbers, symbols):
 def index():
     return render_template("index.html")
 
-@application.route("/generate-password", methods=["POST"])
+@application.route("/generate-password", methods=["GET", "POST"])
 def generate_password():
     
-    length = int(request.form.get("length", 16)) # 16 is default if no value is provided
-    upper = "uppercase" in request.form
-    lower = "lowercase" in request.form
-    numbers = "numbers" in request.form
-    symbols = "symbols" in request.form
     
-    try:
-        password = generate_random_password(length, upper, lower, numbers, symbols)
-        return render_template("index.html", generated_password=password)
+    if request.method == "POST":
+        length = int(request.form.get("length", 16)) # 16 is default if no value is provided
+        upper = "uppercase" in request.form
+        lower = "lowercase" in request.form
+        numbers = "numbers" in request.form
+        symbols = "symbols" in request.form
+        
+        try:
+            password = generate_random_password(length, upper, lower, numbers, symbols)
+            return render_template("index.html", generated_password=password)
 
-    except IndexError:
-        flash("Please click a checkbox below to add options for password generation!")
-        return render_template("index.html")
+        except IndexError:
+            return render_template("index.html")
+    else:
+        return redirect("/")
 
-@application.route("/check-strength", methods=["POST"])
+@application.route("/check-strength", methods=["GET", "POST"])
 def check_strength():
-    return render_template("index.html")
+    
+    if request.method == "POST":
+        password = request.form.get("password_to_check")
+        strength = 0
+        strength_feedback = "Please enter a password above"
+        
+        upper, lower, numbers, symbols = False, False, False, False
+        
+        if password:
+            for char in password:
+                if char.isupper():
+                    upper = True
+                if char.islower():
+                    lower = True
+                if char in string.punctuation:
+                    symbols = True
+                if char in string.digits:
+                    numbers = True
+            
+            if upper:
+                strength += .15
+            if lower: 
+                strength += .15
+            if numbers:
+                strength += .15
+            if symbols:
+                strength += .15
+            
+            print(strength)
+            
+            length_modifier = (len(password) / 12) * .5 # 12 is max password length strength modifier
+            
+            if len(password) < 6:
+                strength = .25
+                strength_feedback = "weak"
+            else:
+                strength += length_modifier
+                
+                if strength <= 0.3:
+                    strength_feedback = "Weak"
+                    color = "bg-red-500"
+                elif strength <= 0.5:
+                    strength_feedback = "Fair"
+                    color = "bg-orange-500"
+                elif strength <= 0.75:
+                    strength_feedback = "Strong"
+                    color = "bg-green-500"
+                elif strength > 1:
+                    strength = 1
+                    strength_feedback = "Very Strong"
+                    color = "bg-emerald-500"
+    
+        return render_template("index.html", password=password, strength_bar_value=strength * 100, strength_bar_color=color, strength_feedback=strength_feedback)
+    
+    else:
+        return redirect("/")
